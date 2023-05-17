6b4c878bcdb71e024334368b170e6e3e83f7041c
Ho Tuan Khai <khaihtgcd210244@fpt.edu.vn>
2023-05-17 15:06:28 +0700

    Day 2 web modified

--- webapp.js
+++ webapp.js
@@ -4,8 +4,18 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader("Content-Type", "text/html");
-res.end("<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>"
-);
+switch (req.url) {
+case "/home":
+res.writeHead(200);
+res.end("<h1>This is Home page</h1>");
+break;
+case "/about":
+res.writeHead(200);
+res.end("<h1>This is About page</h1>");
+break;
+default:
+break;
+}
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
